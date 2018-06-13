e457f3851e6edf381eb0507ad2efb1fd8849e760
Suelliton Santos <tonmelodicmetal@gmail.com>
2018-06-13 15:28:20 -0300

    Update HidroTechControl.ino

--- HidroTechControl.ino
+++ HidroTechControl.ino
@@ -1,57 +1,66 @@
-#define condutividade  A0
-#define nivelAlto      8
-#define nivelBaixo     9
-#define bombaPrincipal 7 //relé
-#define bombaDescarte  4 //relédescarte
+#define condutividade      0
+#define nivelAlto          9
+#define nivelAltoDescarte  10
+#define nivelBaixo         8
+#define bombaPrincipal     7 //relé
+#define bombaDescarte      4 //relédescarte
 
-bool switchBomba = false;
-bool flag = false;
+bool switchBomba     =     false;
+bool flag            =     false;
 
-long previousMillis = 0;        
-long timeOutColeta = 20000; 
+long previousMillis  =     0;        
+long timeOutColeta   =     5000; 
 
 
 void controlaBombas(bool flag);
 
 void setup() {
     Serial.begin(9600);
-    pinMode(nivelAlto,      INPUT);
-    pinMode(nivelBaixo,     INPUT);
-    pinMode(condutividade, INPUT);
-    pinMode(bombaPrincipal, OUTPUT);
-    pinMode(bombaDescarte,  OUTPUT);
+    pinMode(nivelAlto,          INPUT);
+    pinMode(nivelBaixo,         INPUT);
+    //pinMode(condutividade,      INPUT);
+    pinMode(bombaPrincipal,     OUTPUT);
+    pinMode(bombaDescarte,      OUTPUT);
     digitalWrite(bombaPrincipal,LOW);
-    digitalWrite(bombaDescarte,LOW);
+    digitalWrite(bombaDescarte, LOW);
 }
 
 void loop() {
     unsigned long currentMillis = millis();
     bool valorNivelBaixo = digitalRead(nivelBaixo);
+
+    int valorCondutividade = analogRead(condutividade);//map(analogRead(condutividade), 1023, 0, 0, 1023);
     
-    int valorCondutividade = map(analogRead(condutividade), 1023, 0, 0, 1023);
-    //controle das bombas por meio dos relés com base no sensor de condutividade
+    Serial.print("condutividade: ");
     Serial.println(valorCondutividade);
-    if (valorCondutividade >= 512) {
+       
+    if (valorCondutividade <= 150) {
         //ficou choca
-        digitalWrite(bombaDescarte,  true); //desativa a bomba principal
-    } else {
-        digitalWrite(bombaDescarte,  false);
-        if (currentMillis - previousMillis >= timeOutColeta) { //Verifica se o tempo desgotou
-            previousMillis = currentMillis;  
-            switchBomba = !switchBomba; 
-            digitalWrite(bombaPrincipal, switchBomba); 
+        while(analogRead(condutividade)<=150){
+          digitalWrite(bombaDescarte,  true); //desativa a bomba principal
+          digitalWrite(bombaPrincipal,false);
+
+          Serial.print("analog: ");
+          Serial.println(analogRead(condutividade));
         }
-//        if (!valorNivelBaixo){
-//            //ativa a solenóide
-//            controleBombas(1, false);
-//            controleBombas(2, false);
-//        }
-//        //else ativa uma solenóide para encher o tanque principal
-//        else if (digitalRead(nivelAlto)) {
-//            //desativar a solenóide
-//            controlebombas(1, true);
-//        }
+    } else {            
+            if (!valorNivelBaixo){
+                  digitalWrite(bombaPrincipal, false);
+                  digitalWrite(bombaDescarte, false);   
+                   Serial.println("desligou as duas");           
+            } else {            
+                  if (currentMillis - previousMillis >= timeOutColeta ) { //Verifica se o tempo desgotou
+                      previousMillis = currentMillis;             
+                      switchBomba = !switchBomba; 
+                      digitalWrite(bombaPrincipal, switchBomba);  
+                      digitalWrite(bombaDescarte,  false);  
+                      Serial.print("estado bomba");
+                      Serial.println(switchBomba);
+                  }
+              }
     }
-    //
+      Serial.println("------------------------------------------------");
+
+    
 
 }
